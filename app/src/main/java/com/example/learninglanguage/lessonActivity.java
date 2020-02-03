f52ccd05dee0b1f4a801c89b332e473b4b343b3d
prabhjotnarwal <44123405+prabhjotnarwal@users.noreply.github.com>
2020-02-02 19:31:45 -0500

    register,login & main page

---
+++ app/src/main/java/com/example/learninglanguage/lessonActivity.java
@@ -0,0 +1,14 @@
+package com.example.learninglanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson);
+    }
+}
