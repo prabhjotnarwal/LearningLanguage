7ffddb485d6bd6c102bbf46577e6277d7ff91f8d
FairuzKhan <fairuzk15@gmail.com>
2020-02-19 18:55:57 -0500

    Changes for Advance level video addition

---
+++ app/src/main/java/com/example/learninglanguage/AdvanceLevel.java
@@ -0,0 +1,64 @@
+package com.example.learninglanguage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class AdvanceLevel extends AppCompatActivity {
+
+    VideoView video;
+    TextView url;
+    ProgressDialog pd;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference reference = firebaseDatabase.getReference();
+    private DatabaseReference childreference = reference.child("url");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advance_level);
+        video = (VideoView) findViewById(R.id.video);
+        pd = new ProgressDialog(AdvanceLevel.this);
+        url = (TextView) findViewById(R.id.txt1);
+        pd.setMessage("Buffering please wait");
+        pd.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        childreference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String message = dataSnapshot.getValue(String.class);
+                Uri uri = Uri.parse(message);
+                video.setVideoURI(uri);
+                video.start();
+                video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        pd.dismiss();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
