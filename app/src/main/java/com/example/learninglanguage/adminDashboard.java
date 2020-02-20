15732d5a3e7915d02d2d7b701e337983aba14535
prabhjotnarwal <44123405+prabhjotnarwal@users.noreply.github.com>
2020-02-20 18:07:29 -0500

    admin dashboard and text sizes

--- app/src/main/java/com/example/learninglanguage/adminDashboard.java
+++ app/src/main/java/com/example/learninglanguage/adminDashboard.java
@@ -3,12 +3,16 @@ package com.example.learninglanguage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class adminDashboard extends AppCompatActivity {
-
+Button b1,b2,b3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        b1=findViewById(R.id.cAdd);
+        b2=findViewById(R.id.lAdd);
+        b3=findViewById(R.id.tAdd);
     }
 }
