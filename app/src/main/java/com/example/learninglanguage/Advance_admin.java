85c531feb13c87df8af949cc4b8c1e364751bec6
kaur1894831 <gillgulbeer@gmail.com>
2020-02-22 14:12:39 -0500

    upload video file

--- app/src/main/java/com/example/learninglanguage/Advance_admin.java
+++ app/src/main/java/com/example/learninglanguage/Advance_admin.java
@@ -9,6 +9,6 @@ public class Advance_admin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_advance_admin);
+        setContentView(R.layout.activity_advance_admin_1);
     }
 }
