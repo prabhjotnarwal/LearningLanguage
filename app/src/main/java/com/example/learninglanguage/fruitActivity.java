06f23653165274dfba6bd0502cb83a4e2f19b0c7
lovepreet <lovepreetkaur3105>
2020-02-25 12:09:22 -0800

    fruit activity for lesson test changes

--- app/src/main/java/com/example/learninglanguage/fruitActivity.java
+++ app/src/main/java/com/example/learninglanguage/fruitActivity.java
@@ -3,9 +3,12 @@ package com.example.learninglanguage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class fruitActivity extends AppCompatActivity {
-
+private Button ba,org,ap,ki,st,man,gr,wm;
+private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
