0351984458626e420bc2d68048684d8a920050cc
lovepreet <lovepreetkaur3105>
2020-02-27 00:16:19 -0800

    fruit activity lessons updations

--- app/src/main/java/com/example/learninglanguage/fruitActivity.java
+++ app/src/main/java/com/example/learninglanguage/fruitActivity.java
@@ -7,11 +7,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class fruitActivity extends AppCompatActivity {
-private Button ba,org,ap,ki,st,man,gr,wm;
+private Button banana,orange,apple,kiwi,stberry,mango,grapes,wmelon;
 private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fruit);
+        img = findViewById(R.id.cimage);
+        banana = findViewById(R.id.ba);
+        orange = findViewById(R.id.org);
+        apple = findViewById(R.id.ap);
+        kiwi = findViewById(R.id.ki);
+        stberry = findViewById(R.id.st);
+        mango = findViewById(R.id.man);
+        grapes = findViewById(R.id.gr);
+        wmelon = findViewById(R.id.wm);
     }
 }
